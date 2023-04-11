710db782165d18606d4f810e1ac1b215de136f24
NasraYonis <nasra@teamalyx.com>
2023-04-11 11:48:11 +0200

    First commit

---
+++ Desktop Connect Four/task/src/four/ConnectFour.java
@@ -0,0 +1,37 @@
+package four;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ConnectFour extends JFrame {
+    private static final int ROWS = 6;
+    private static final int COLS = 7;
+
+    public ConnectFour() {
+        super("Connect Four");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        JPanel board = new JPanel(new GridLayout(ROWS + 1, COLS));
+        JLabel[] labels = {new JLabel("A"), new JLabel("B"), new JLabel("C"),
+                new JLabel("D"), new JLabel("E"), new JLabel("F"),
+                new JLabel("G")};
+        for (JLabel label : labels) {
+            label.setHorizontalAlignment(SwingConstants.CENTER);
+            board.add(label);
+        }
+        for (int row = 1; row <= ROWS; row++) {
+            for (int col = 0; col < COLS; col++) {
+                JButton cell = new JButton();
+                cell.setFocusPainted(false);
+                cell.setName("Button" + row + (char) ('A' + col));
+                cell.setText((char) ('A' + col) + "" + row);
+                board.add(cell);
+            }
+        }
+        add(board);
+
+        pack();
+        setVisible(true);
+
+    }
+}
